5ece6ad048a99779d78de0ea10d64fa860fe6f2b
Charlie Gunyon <charles.gunyon@gmail.com>
2014-08-19 12:53:08 -0400

    Bump the console/messages font down 1

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 export LIBGL_ALWAYS_SOFTWARE=1
+CMD="cbuild/doom2k -iwad freedoom2.wad -file freedm.wad -warp 1 -nomonsters"
 
-gdb -ex 'source gdbsetup.txt' --args cbuild/doom2k -iwad freedoom2.wad -file freedm.wad -nomouse -warp 1 -nomonsters -nodraw
+gdb -ex run --args $CMD
 # CPUPROFILE=cpu.prof cbuild/doom2k
 # cbuild/doom2k -skill 4 -warp 1 -file sunder.wad -warp 6
 # cbuild/doom2k -playdemo DEMO1
