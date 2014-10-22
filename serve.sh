5d1a593a7fcecd6045f45e6f2ed46d41f3b80c7f
Charlie Gunyon <charles.gunyon@gmail.com>
2014-10-22 19:29:52 -0400

    Re-enable range coder, bandwidth win is huge, so it's worth the CPU

--- serve.sh
+++ serve.sh
@@ -3,6 +3,8 @@ WAD="-file heights2.wad"
 WAD=""
 CMD="cbuild/doom2k $WAD -skill 5 -warp 1 -nomonsters -deathmatch -frags 5 -deltaserve"
 CMD="cbuild/doom2k $WAD -skill 4 -warp 1 -deltaserve"
+CMD="cbuild/doom2k $WAD -skill 5 -warp 1 -deathmatch -frags 5 -deltaserve"
+CMD="cbuild/doom2k $WAD -skill 4 -warp 1 -nomonsters -deltaserve"
 
 # gdb -ex 'source gdbserver.txt' --args $CMD
 gdb -ex run --args $CMD
