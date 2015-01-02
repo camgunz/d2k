06a45f743681211fc321592143f8a48a84c01cf2
Charlie Gunyon <charles.gunyon@gmail.com>
2015-01-02 17:30:59 -0500

    Fix problem with version in window caption on Windows (for real this time)
    Working on pangocairo rendering in console.lua

--- scripts/console.lua
+++ scripts/console.lua
@@ -21,9 +21,9 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
--- local cairo = require('lgob.cairo')
--- local pango = require('lgob.pango')
--- local pangocairo = require('lgob.pangocairo')
+local cairo = require('lgob.cairo')
+local pango = require('lgob.pango')
+local pangocairo = require('lgob.pangocairo')
 local hud_widget = require('hud_widget')
 
 Console = hud_widget.HUDWidget:new()
@@ -38,7 +38,7 @@ end
 function Console:tick()
 end
 
-function Console:draw()
+function Console:old_draw()
   local xc = 80
   local yc = 60
   local radius = 30
@@ -58,6 +58,52 @@ function Console:draw()
   d2k.overlay.context:arc(xc, yc, radius, angle2, angle2)
   d2k.overlay.context:line_to(xc, yc)
   d2k.overlay.context:stroke()
+end
+
+function Console:draw()
+  local pango_context = pangocairo.create_context(d2k.overlay.context)
+  local pango_layout = pango.Layout.new(pango_context)
+
+  -- local font_description = pango.FontDescription.from_string('Sans Bold 27')
+
+  -- layout:set_font_description(font_description)
+  -- pango_layout:set_text("x", 1)
+  pangocairo.update_context(d2k.overlay.context, pango_context)
+  pangocairo.update_layout(d2k.overlay.context, pango_layout)
+
+  pangocairo.show_layout(d2k.overlay.context, pango_layout)
+end
+
+function Console:new_draw()
+  local word_count = 9
+  local font_description = pango.FontDescription.from_string('Sans Bold 27')
+  local width = d2k.get_screen_width()
+  local height = d2k.get_screen_height()
+  local radius = (width < height and width or height) / 2
+  local layout = pangocairo.create_layout(d2k.overlay.context)
+
+  layout:set_text("Text", -1)
+  layout:set_font_description(font_description)
+  d2k.overlay.context:translate(radius, radius)
+
+  -- Draw the layout word_count times in a circle
+  for i = 0, word_count do
+    local angle = (360 * i) / word_count
+    -- d2k.overlay.context:save()
+
+    -- Color gradient
+    local red = (1 + math.cos((angle - 60) * math.pi / 180)) / 2
+    d2k.overlay.context:set_source_rgb(red, 0, 1 - red)
+    d2k.overlay.context:rotate(angle * math.pi / 180)
+
+    -- Inform Pango to re-layout the text with the new transformation
+    pangocairo.update_layout(d2k.overlay.context, layout)
+    local width, height = layout:get_size()
+    d2k.overlay.context:move_to(-(width / pango.SCALE) / 2, -radius)
+    pangocairo.show_layout(d2k.overlay.context, layout)
+
+    -- d2k.overlay.context:restore()
+  end
 end
 
 return {Console = Console}
