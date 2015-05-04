ff7bb57d585385c78a78c685e85714e83a9534ba
Charlie Gunyon <charles.gunyon@gmail.com>
2015-05-04 18:38:45 +0000

    Update the cross-building scripts

--- crossbuild.sh
+++ crossbuild.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
+set -e
+
 CROSSDEPS="`pwd`/crossdeps"
 CROSSLIBS="$CROSSDEPS/bin"
 BUILD_DIR="crossbuild"
 BUILD_TYPE="Debug"
 
-cd $BUILD_DIR && \
+if [ ! -d "$BUILD_DIR" ]
+then
+    mkdir "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR" && \
     CPPFLAGS="-I$CROSSDEPS/include" \
     LDFLAGS="-L$CROSSDEPS/lib" \
     SDLDIR="$CROSSDEPS" \
     PKG_CONFIG_PATH="$CROSSDEPS/lib/pkgconfig" \
     cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+             -DLGI_LIBRARIES="$CROSSDEPS/lib/lua/5.2/lgi/corelgilua51.dll" \
              -DCMAKE_TOOLCHAIN_FILE=mingw32-toolchain.cmake && \
     clear && make
 
--- install_mingw64_packages.sh
+++ install_mingw64_packages.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
-yaourt -S mingw-w64-gcc       \
-          mingw-w64-enet      \
-          mingw-w64-libxdiff  \
-          mingw-w64-json-c    \
-          mingw-w64-libwebp   \
-          mingw-w64-dumb      \
-          mingw-w64-libmad    \
-          mingw-w64-sdl_mixer \
-          mingw-w64-sdl_image \
-          mingw-w64-gtk3
+yaourt --noconfirm -S mingw-w64-gcc       \
+                      mingw-w64-libxdiff  \
+                      mingw-w64-libmad    \
+                      mingw-w64-sdl_mixer \
+                      mingw-w64-sdl_image \
+                      mingw-w64-enet      \
+                      mingw-w64-json-c    \
+                      mingw-w64-dumb      \
+                      mingw-w64-libwebp   \
+                      mingw-w64-xz        \
+                      mingw-w64-c-ares    \
+                      mingw-w64-curl
 
 ###
-#
 # Still requires:
-#   - PolarSSL (https://polarssl.org)
 #   - PortMidi (http://portmedia.sourceforge.net/portmidi)
 #   - FluidSynth (http://www.fluidsynth.org)
-#   - Pango (http://pango.org)
-#     - Package is not built against a static GLib
-#   - GObject Introspection (http://live.gnome.org/GObjectIntrospection)
-#   - Lua 5.1 (http://www.lua.org)
-#     - Package is built using g++ rather than GCC
-#   - lgob (https://bitbucket.org/lucashnegri/lgib)
-#
 ###
 
