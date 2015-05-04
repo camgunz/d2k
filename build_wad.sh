bf9817de375626934e40d5f2ed81fa8a03cf65f2
Charlie Gunyon <charles.gunyon@gmail.com>
2015-05-04 19:02:19 +0000

    Have crossbuild.sh build a distribution folder; use set -e in build_wad.sh

--- build_wad.sh
+++ build_wad.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 if [ ! `which deutex 2> /dev/null` ]
 then
     echo 'Error: deutex not found, cannot build WAD'
     exit 1
 fi
 
-cd data && deutex -make d2k.txt d2k.wad
+cd data
+deutex -make d2k.txt d2k.wad
 
